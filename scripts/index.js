511d43a7255c07c9b605f72c7097ae10ef538a77
Julia Zabolotnaya <yuv.zabolotnaya@yandex.by>
2021-10-07 17:29:04 +0300

    Init js logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,68 @@
 const menuButton = document.querySelector('.menu-button');
+const header = document.querySelector('.header');
+const siteNav = document.querySelector('.navigation');
+const themeToggleButton = document.querySelector('.theme-toggle__button');
+const themeBox = document.querySelector('.theme-toggle');
+const form = document.forms.subscription;
+const input = form.elements['user-email'];
 
-menuButton.addEventListener('click', () => {
+function setListeners() {
+  addThemeListener();
+  addNavMenuListener();
+  addScrollListener();
+  addFormListener();
+}
+
+function addThemeListener() {
+  themeToggleButton.addEventListener('change', handleThemeToggle);
+}
+
+function handleThemeToggle(e) {
+  e.target.checked
+    ? document.documentElement.classList.add('dark-theme')
+    : document.documentElement.classList.remove('dark-theme');
+}
+
+function addNavMenuListener() {
+  menuButton.addEventListener('click', handleNavMenuToggle);
+}
+
+function handleNavMenuToggle(e) {
+  document.body.classList.toggle('page_menu-opened');
+  siteNav.classList.toggle('navigation_opened');
   menuButton.classList.toggle('menu-button_opened');
-});
+  themeBox.classList.toggle('theme-toggle_header');
+}
+
+function addScrollListener() {
+  window.addEventListener('scroll', handleSiteScroll);
+}
+
+function handleSiteScroll() {
+  window.pageYOffset > 100
+    ? header.classList.add('header_scrolled')
+    : header.classList.remove('header_scrolled');
+}
+
+function addFormListener() {
+  form.addEventListener('submit', handleSubmit);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const email = input.value.trim();
+
+  if (email) {
+    input.value = 'Круто!';
+    input.blur();
+
+    setTimeout(clearInputValue, 3000);
+  }
+}
+
+function clearInputValue() {
+  input.value = '';
+}
+
+setListeners();
